d0232691a4ca685ca68c29ea6961d8fe13eae9af
Thida CHHEANG <tchheang09@univic.fr>
2019-12-03 15:19:33 +0100

    Add form

---
+++ view/template/form.php
@@ -0,0 +1,28 @@
+<div id="formulaire">
+	<div id="form1">
+		<h1>Bonjour</h1>
+		<form>
+			<div class="form-group">
+				<textarea class="form-control" id="post" placeholder="Quoi de neuf ?" rows="5"></textarea>
+			</div>
+			<div class="form-group">
+				<label for="file">Ajouter une image</label>
+				<input type="file" class="form-control-file" id="file">
+			</div>
+			<button type="submit" class="btn btn-primary">Poster</button>
+		</form>
+	</div>
+	<div id="form2">
+		<div id="username">prenom.nom</div>
+		<div id="date">2019-12-03</div>
+		<div id="post">Bonjour à tous</div>
+		<form>
+			<div>
+				<button type="button" class="btn btn-primary">Aimer</button>
+				<div id="counter">0</div>
+				<button type="button" class="btn btn-primary">Commenter</button>
+			</div>
+			<textarea class="form-control" id="comment" placeholder="Votre commentaire ?" rows="3"></textarea>
+		</form>
+	</div>
+</div>
