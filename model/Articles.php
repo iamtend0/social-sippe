cff9baf56a8b76ebe475c7d1c0ed0fa8c337120c
Thida CHHEANG <tchheang09@univic.fr>
2019-12-17 14:02:54 +0100

    Model Articles

--- model/Articles.php
+++ model/Articles.php
@@ -6,3 +6,30 @@
  * and open the template in the editor.
  */
 
+$servername = "localhost";
+$dbname = "social_sippe";
+$username = "root";
+$password = "";
+$dbh = new PDO('mysql:host='.$servername.';dbname='.$social_sippe, 
+                        $username, $password);
+
+/**
+ * 
+ * @param type $message
+ * @param type $linkImg
+ * @param type $date
+ * @param type $user
+ * @return 
+ */
+function publish($message, $linkImg, $date, $user) {
+
+	$errorMessage = '';
+	if($message == '') {
+		$errorMessage = "Ecrivez un message"
+	}
+
+	if($errMsg == '') {
+		
+    }
+
+}
