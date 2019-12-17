f0e85c377a8dc13495e9138eed7a361539613071
Thida CHHEANG <tchheang09@univic.fr>
2019-12-17 15:40:58 +0100

    Modif Articles

--- model/Articles.php
+++ model/Articles.php
@@ -17,19 +17,120 @@ $dbh = new PDO('mysql:host='.$servername.';dbname='.$social_sippe,
  * 
  * @param type $message
  * @param type $linkImg
- * @param type $date
  * @param type $user
  * @return 
  */
-function publish($message, $linkImg, $date, $user) {
+function publish($message, $linkImg, $user) {
 
 	$errorMessage = '';
 	if($message == '') {
-		$errorMessage = "Ecrivez un message"
+		$errorMessage = "Ecrivez un message."
 	}
 
-	if($errMsg == '') {
-		
+	if($errorMessage == '') {
+		try {
+			$date = date("Y-m-d");
+			$stmt = $dbh->prepare('INSERT INTO articles (ref_user, link_img, description, date_publication) VALUES (:ref_user, :link_img, :description, :date_publication)');
+            $stmt->execute(array(
+                ':ref_user' => $ref_user,
+                ':link_img' => $link_img,
+                ':description' => $description,
+                ':date_publication' => $date_publication
+            ));
+            $errorMessage = "Succès."
+        } catch(PDOException $e) {
+			$errorMessage = $e->getMessage();
+        }
     }
+    
+    return $errorMessage;
+
+}
+
+/**
+ * 
+ * @param type $nbPosts
+ * @return 
+ */
+function fetchArticles($nbPosts) {
+
+	$errorMessage = '';
+
+	if($errorMessage == '') {
+		try {
+			$stmt = $dbh->prepare('SELECT * FROM articles LIMIT :nbPosts');
+            $stmt->execute(array(
+                ':nbPosts' => $nbPosts
+            ));
+            $data = $stmt->fetchAll();
+            if($data == false){
+                $errorMessage = "Erreur";
+            } else {
+                return $data;
+            }
+        } catch(PDOException $e) {
+			$errorMessage = $e->getMessage();
+			return $errorMessage;
+        }
+    }
+
+}
+
+/**
+ * 
+ * @param type $user
+ * @param type $idArticle
+ * @return 
+ */
+function like($user, $idArticle) {
+
+	$errorMessage = '';
+
+	if($errorMessage == '') {
+		try {
+			$date = date("Y-m-d");
+			$stmt = $dbh->prepare('INSERT INTO likes (ref_user, ref_article, date_like) VALUES (:ref_user, :ref_article, :date_like)');
+            $stmt->execute(array(
+                ':ref_user' => $user,
+                ':ref_article' => $idArticle,
+                ':date_like' => $date
+            ));
+            $errorMessage = "Succès."
+        } catch(PDOException $e) {
+			$errorMessage = $e->getMessage();
+        }
+    }
+    
+    return $errorMessage;
+
+}
+
+/**
+ * 
+ * @param type $idArticle
+ * @return 
+ */
+function getLikes($idArticle) {
+
+	$errorMessage = '';
+
+	if($errorMessage == '') {
+		try {
+			$stmt = $dbh->prepare('SELECT COUNT(*) FROM likes WHERE ref_article = :ref_article');
+            $stmt->execute(array(
+                ':ref_article' => $idArticle
+            ));
+            $data = $stmt->fetchAll();
+            if($data == false){
+                $errorMessage = "Erreur";
+            } else {
+                $errorMessage = $data;
+            }
+        } catch(PDOException $e) {
+			$errorMessage = $e->getMessage();
+        }
+    }
+    
+    return $errorMessage;
 
 }
